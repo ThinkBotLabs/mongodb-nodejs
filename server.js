0110d0d793250d9453d5f1df1f45b71256dd01d0
aaron <aaron@thinkbotlabs.com>
2019-11-20 20:59:21 -0600

    Initial Check In. NodeJS with a MongoDb connection

---
+++ server.js
@@ -0,0 +1,55 @@
+//use express
+const express = require("express");
+const bodyParser = require("body-parser")
+const MongoClient = require("mongodb").MongoClient
+const app = express()
+var db
+
+app.set('view engine', 'ejs')
+
+const options = {
+    keepAlive: 1,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+
+}
+
+MongoClient.connect("mongodb+srv://aaron:9MyY$ag2hZABx@pj@cluster0-cecpd.mongodb.net/test?retryWrites=true&w=majority", options,  (err, MongoClient) => 
+{   
+    if (err) return console.log(err)
+
+    //name of the database in quotes
+    db = MongoClient.db("cluster0-cecpd")
+
+
+    //add a listener so browsers can connect to us
+    app.listen(4200, function ()
+    {
+        console.log("listening on port 4200")
+    })
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) =>
+{
+    db.collection('quotes').find().toArray((err, result) =>
+    {
+        if (err) return console.log(err)
+        // renders index.ejs
+        res.render('index.ejs', { quotes: result })
+    })
+})
+
+app.post('/quotes', (req, res) =>
+{
+    db.collection('quotes').insertOne(req.body, (err, result) =>
+    {
+        if (err) return console.log(err)
+
+        console.log('added ' + "'<todo: get object name that we are adding>'" + ' to database')
+
+        res.redirect('/')
+    })
+})
+
